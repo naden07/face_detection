149dfc0ff89292dfdc6e44a917ff79a91a701926
jlnshly <jelaineashleyramos@gmail.com>
2026-03-03 21:56:55 +0800

    change variable names to descriptive style

--- main_program.py
+++ main_program.py
@@ -5,16 +5,16 @@ frontal_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 video_capture = cv2.VideoCapture(0)
 
 while True:
-    c_rec, d_image = video_capture.read()
-    e = cv2.cvtColor(d_image, cv2.COLOR_BGR2GRAY)
-    f = frontal_face.detectMultiScale(e, 1.3,6)
+    capture_success, frame = video_capture.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detected_faces = frontal_face.detectMultiScale(gray_frame, 1.3, 6)
 
-    for (x1,y1, w1, h1) in f:
-        cv2.rectangle(d_image, (x1, y1), (x1+w1, y1+h1), (255,0,0),5)
+    for (x1,y1, w1, h1) in detected_faces:
+        cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (255,0,0),5)
 
-    cv2.imshow('img', d_image)
-    h = cv2.waitKey(40) & 0xFF
-    if h == 40:
+    cv2.imshow('img', frame)
+    pressed_key = cv2.waitKey(40) & 0xFF
+    if  pressed_key== 40:
         break
 
 video_capture.release()
